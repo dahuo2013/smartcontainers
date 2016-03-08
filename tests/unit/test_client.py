a84dc312044fb159184c2d8fc2c1141d31e31966
charlesvardeman <charles.vardeman@gmail.com>
2016-03-07 20:51:03 -0500

    Re-enabling some client tests.

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -43,22 +43,22 @@ def createClient():
 def test_Client(createClient):
     createClient.info()
 
-#def test_simple_tar():
-#    #Create the smartcontainer client
-#    myClient = client.scClient()
-#    #Create a test file to be turned into a tar
-#    with open('tempprov.txt', 'a') as provfile:
-#        provfile.write('This is the data for the tar file test.')
-#    #Call the simple tar function and test the result
-#    thisfile = myClient.simple_tar('tempprov.txt')
-#    assert tarfile.is_tarfile(thisfile.name)
+def test_simple_tar(createClient):
+    #Create the smartcontainer client
+    #myClient = client.scClient()
+    #Create a test file to be turned into a tar
+    with open('tempprov.txt', 'a') as provfile:
+        provfile.write('This is the data for the tar file test.')
+    #Call the simple tar function and test the result
+    thisfile = createClient.simple_tar('tempprov.txt')
+    assert tarfile.is_tarfile(thisfile.name)
 
-#def test_hasProv():
-#    myClient = client.scClient()
-#    newContainer = myClient.dcli.create_container(image='phusion/append', command="/bin/bash", tty=True)
-#    ContainerID = str(newContainer['Id'])
-#    myClient.dcli.start(ContainerID)
-#    assert myClient.hasProv(ContainerID, 'SCProv.jsonld', '/SmartContainer/')
-#    time.sleep(1)
-#    myClient.dcli.stop(ContainerID)
-#    myClient.dcli.remove_container(ContainerID)
+def test_hasProv(createClient):
+    # myClient = client.scClient()
+    newContainer = createClient.create_container(image='phusion/baseimage', command="/bin/bash", tty=True)
+    ContainerID = str(newContainer['Id'])
+    createClient.start(ContainerID)
+    #assert createClient.hasProv(ContainerID, 'SCProv.jsonld', '/SmartContainer/')
+    time.sleep(1)
+    createClient.stop(ContainerID)
+    createClient.remove_container(ContainerID)
