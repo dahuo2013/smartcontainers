566dc74d39d6d2e0b8ce9d3c063df89c1da3e506
charlesvardeman <charles.vardeman@gmail.com>
2016-03-10 18:24:28 -0500

    Working on client.py tests.

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -1,6 +1,7 @@
 import pytest
 import tarfile
 import time
+import os
 
 def test_Client(createClient):
     createClient.info()
@@ -15,12 +16,45 @@ def test_simple_tar(createClient):
     thisfile = createClient.simple_tar('tempprov.txt')
     assert tarfile.is_tarfile(thisfile.name)
 
-def test_hasProv(createClient, pull_docker_image):
-    # myClient = client.scClient()
-    newContainer = createClient.create_container(image='phusion/baseimage', command="/bin/bash", tty=True)
+def test_fileCopyIn(createClient,pull_docker_image):
+    newContainer = createClient.create_container(image='phusion/baseimage:latest', 
+                                                 command="/bin/bash", tty=True)
     ContainerID = str(newContainer['Id'])
     createClient.start(ContainerID)
-    #assert createClient.hasProv(ContainerID, 'SCProv.jsonld', '/SmartContainer/')
+    with open('SCProv.jsonld', 'a') as provfile:
+        provfile.write('This is the data for the tar file test.')
+    createClient.fileCopyIn(ContainerID,'SCProv.jsonld','/SmartContainer/')
+    assert createClient.hasProv(ContainerID, 'SCProv.jsonld', '/SmartContainer/')
     time.sleep(1)
     createClient.stop(ContainerID)
     createClient.remove_container(ContainerID)
+    os.remove('SCProv.jsonld')
+
+#def test_fileCopyOut(createClient):
+#    newContainer = createClient.create_container(image='phusion/append', command="/bin/bash", tty=True)
+#    ContainerID = str(newContainer['Id'])
+#    createClient.start(ContainerID)
+#    createClient.fileCopyOut(ContainerID, 'SCProv.jsonld', '/SmartContainer/')
+#    assert os.path.isfile('SCProv.jsonld')
+#    time.sleep(1)
+#    createClient.stop(ContainerID)
+#    createClient.remove_container(ContainerID)
+#    os.remove('SCProv.jsonld')
+
+#def test_hasProv(createClient, pull_docker_image):
+    # myClient = client.scClient()
+#    newContainer = createClient.create_container(image='phusion/baseimage', command="/bin/bash", tty=True)
+#    ContainerID = str(newContainer['Id'])
+#    createClient.start(ContainerID)
+#    assert createClient.hasProv(ContainerID, 'SCProv.jsonld', '/SmartContainer/')
+#    time.sleep(1)
+#    createClient.stop(ContainerID)
+#    createClient.remove_container(ContainerID)
+
+#def test_put_label_image(createClient):
+#    myLabel = {'smartcontainer':'{"author":"Scott B. Szakonyi"}'}
+#    createClient.put_label_image(imageID='f7874cea1543', label=myLabel, repository='Test', author=None, conf=None, tag=None, message=None)
+#    myInspect = createClient.inspect_image('Test')
+#    assert 'Szakonyi' in str(myInspect)
+#    createClient.remove_image('Test')
+
